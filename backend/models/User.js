4fcfeb851db7d62e563b2ec01e39f03f73e2f968
BATtechie <yuvrajchandravansi2020@gmail.com>
2025-12-31 02:48:13 +0530

    resolve error in api

--- backend/models/User.js
+++ backend/models/User.js
@@ -34,11 +34,10 @@ const userSchema = new mongoose.Schema({
 // Hash password before saving
 userSchema.pre('save', async function(next) {
     if (!this.isModified('password')) {
-        return next();
-    }
+        return next()
+    };
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
 });
 
 // Compare entered password with hashed password
